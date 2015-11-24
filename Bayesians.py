fd8e73da183e7894955a3c3adbd943be754fcf39
dantmunro <dantmunro@gmail.com>
2015-11-24 11:36:32 -0600

    Copied ParticleFilter

--- Bayesians.py
+++ Bayesians.py
@@ -174,12 +174,17 @@ class DefensiveReflexAgent(ReflexCaptureAgent):
     enemies = [successor.getAgentState(i) for i in self.getOpponents(successor)]
     invaders = [a for a in enemies if a.isPacman and a.getPosition() != None and myState.scaredTimer==0]
     features['numInvaders'] = len(invaders)
+    '''
     if len(invaders) > 0 and myState.scaredTimer==0:
       dists = [self.getMazeDistance(myPos, a.getPosition()) for a in invaders]
       if myState.scaredTimer==0:
           features['invaderDistance'] = min(dists)
       else:
           features['invaderDistance'] = min(dists)*-1
+    '''
+    dists = [] #Particle filter - if we know the exact location, move the particles to the spot it's at
+
+    particleFilter = JointParticleFilter()
 
     if action == Directions.STOP: features['stop'] = 1
     rev = Directions.REVERSE[gameState.getAgentState(self.index).configuration.direction]
@@ -189,3 +194,122 @@ class DefensiveReflexAgent(ReflexCaptureAgent):
 
   def getWeights(self, gameState, action):
     return {'numInvaders': -1000, 'distanceToFood': -2, 'onDefense': 100, 'invaderDistance': -10, 'stop': -100, 'reverse': -2, 'distanceToCapsule': -5}
+
+# The JointParticleFilter and getObservationDistribution method (and associated constants) were copied from project4
+class JointParticleFilter:
+  "JointParticleFilter tracks a joint distribution over tuples of all ghost positions."
+
+  def __init__(self, numParticles=600):
+     self.setNumParticles(numParticles)
+
+  def setNumParticles(self, numParticles):
+    self.numParticles = numParticles
+
+  def initialize(self, gameState, legalPositions):
+    "Stores information about the game, then initializes particles."
+    self.numGhosts = gameState.getNumAgents() - 1
+    self.ghostAgents = []
+    self.legalPositions = legalPositions
+    self.initializeParticles()
+
+  def initializeParticles(self):
+    "Initializes particles randomly.  Each particle is a tuple of ghost positions. Use self.numParticles for the number of particles"
+    self.particles = [tuple([random.choice(self.legalPositions) for _ in range(self.numGhosts)] + [1]) for _ in range(self.numParticles)]
+
+  def addGhostAgent(self, agent):
+    "Each ghost agent is registered separately and stored (in case they are different)."
+    self.ghostAgents.append(agent)
+
+  def elapseTime(self, gameState):
+    """
+    Samples each particle's next state based on its current state and the gameState.
+    """
+    newParticles = []
+    for oldParticle in self.particles:
+      newParticle = list(oldParticle) # A list of ghost positions
+      "*** YOUR CODE HERE ***"
+      for i in range(self.numGhosts):
+        prevGhostPositions = newParticle[:-1]
+        newPosDist = getPositionDistributionForGhost(setGhostPositions(gameState, prevGhostPositions),
+                                                     i, self.ghostAgents[i])
+        newParticle[i] = util.sampleFromCounter(newPosDist)
+      newParticles.append(tuple(newParticle))
+    self.particles = newParticles
+
+  def getJailPosition(self, i):
+    return (2 * i + 1, 1);
+
+  def observeState(self, gameState):
+    """
+    Resamples the set of particles using the likelihood of the noisy observations.
+    """
+    pacmanPosition = gameState.getPacmanPosition()
+    noisyDistances = gameState.getNoisyGhostDistances()
+    if len(noisyDistances) < self.numGhosts: return
+    emissionModels = [busters.getObservationDistribution(dist) for dist in noisyDistances]
+
+    "*** YOUR CODE HERE ***"
+    WEIGHT = -1
+    self.particles = self.sendToJail(noisyDistances)
+    newParticles = []
+    for par in self.particles:
+        newParticle = list(par)
+        newParticle[WEIGHT] = reduce(mul, [emissionModels[i][util.manhattanDistance(pacmanPosition, newParticle[i])]
+                                       for i in range(len(emissionModels)) if noisyDistances[i] != None])
+        newParticles.append(tuple(newParticle))
+    newDistribution = util.Counter()
+    for par in newParticles:
+        newDistribution[par[:WEIGHT]] = sum(newParticle[WEIGHT] for newParticle in newParticles if newParticle[:WEIGHT] == par[:WEIGHT])
+    if len([par for par in newParticles if newDistribution[par[:WEIGHT]] > 0]) == 0:
+        self.initializeParticles()
+        self.particles = self.sendToJail(noisyDistances)
+        newDistribution = util.Counter()
+        for par in self.particles:
+            newDistribution[par[:WEIGHT]] = sum(newParticle[WEIGHT] for newParticle in self.particles if newParticle[:WEIGHT] == par[:WEIGHT])
+    else:
+        self.particles = [tuple(list(util.sampleFromCounter(newDistribution)) + [1]) for _  in range(self.numParticles)]
+
+  #If certain ghosts are meant to be in jail, update the particles accordingly
+  def sendToJail(self, noisyDistances):
+    newParticles = [list(par) for par in self.particles]
+    if any(noisyDistance == None for noisyDistance in noisyDistances):
+        for i in range(len(self.ghostAgents)):
+            if noisyDistances[i] == None:
+                for j in range(len(newParticles)):
+                    newParticles[j][i] = self.getJailPosition(i)
+        return [tuple(par) for par in newParticles]
+    else:
+        return self.particles
+
+  def getBeliefDistribution(self):
+    dist = util.Counter()
+    for part in self.particles: dist[part] += 1
+    dist.normalize()
+    return dist
+
+SONAR_NOISE_RANGE = 15 # Must be odd
+SONAR_MAX = (SONAR_NOISE_RANGE - 1)/2
+SONAR_NOISE_VALUES = [i - SONAR_MAX for i in range(SONAR_NOISE_RANGE)]
+SONAR_DENOMINATOR = 2 ** SONAR_MAX  + 2 ** (SONAR_MAX + 1) - 2.0
+SONAR_NOISE_PROBS = [2 ** (SONAR_MAX-abs(v)) / SONAR_DENOMINATOR  for v in SONAR_NOISE_VALUES]
+
+def getNoisyDistance(pos1, pos2):
+  if pos2[1] == 1: return None
+  distance = util.manhattanDistance(pos1, pos2)
+  return max(0, distance + util.sample(SONAR_NOISE_PROBS, SONAR_NOISE_VALUES))
+
+observationDistributions = {}
+def getObservationDistribution(noisyDistance):
+  """
+  Returns the factor P( noisyDistance | TrueDistances ), the likelihood of the provided noisyDistance
+  conditioned upon all the possible true distances that could have generated it.
+  """
+  global observationDistributions
+  if noisyDistance == None:
+    return util.Counter()
+  if noisyDistance not in observationDistributions:
+    distribution = util.Counter()
+    for error , prob in zip(SONAR_NOISE_VALUES, SONAR_NOISE_PROBS):
+      distribution[max(1, noisyDistance - error)] += prob
+    observationDistributions[noisyDistance] = distribution
+  return observationDistributions[noisyDistance]
