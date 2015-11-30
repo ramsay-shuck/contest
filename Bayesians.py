159adcf5e5139f19d5149c71adb4aebd915f1565
ramsay-shuck <ramsay.shuck@doane.edu>
2015-11-30 16:34:05 -0600

    Added register initial state

--- Bayesians.py
+++ Bayesians.py
@@ -40,6 +40,32 @@ def createTeam(firstIndex, secondIndex, isRed,
 ##########
 
 class ReflexCaptureAgent(CaptureAgent):
+  
+  def registerInitialState(self, gameState):
+    """
+    This method handles the initial setup of the
+    agent to populate useful fields (such as what team
+    we're on).
+
+    A distanceCalculator instance caches the maze distances
+    between each pair of positions, so your agents can use:
+    self.distancer.getDistance(p1, p2)
+
+    IMPORTANT: This method may run for at most 15 seconds.
+    """
+
+    '''
+    Make sure you do not delete the following line. If you would like to
+    use Manhattan distances instead of maze distances in order to save
+    on initialization time, please take a look at
+    CaptureAgent.registerInitialState in captureAgents.py.
+    '''
+    CaptureAgent.registerInitialState(self, gameState)
+
+    '''
+    Your initialization code goes here, if you need any.
+    '''
+
   """
   A base class for reflex agents that chooses score-maximizing actions
   """
